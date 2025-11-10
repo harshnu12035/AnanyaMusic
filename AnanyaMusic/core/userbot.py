15748cfbc2439b283d9491e12beecc8449555328
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-11-10 13:30:01 +0530

    Update userbot.py

--- AnanyaMusic/core/userbot.py
+++ AnanyaMusic/core/userbot.py
@@ -1,3 +1,25 @@
+# Copyright (c) 2025 Akash Daskhwanshi <ZoxxOP>
+# Location: Mainpuri, Uttar Pradesh 
+#
+# All rights reserved.
+#
+# This code is the intellectual property of Akash Dakshwanshi.
+# You are not allowed to copy, modify, redistribute, or use this
+# code for commercial or personal projects without explicit permission.
+#
+# Allowed:
+# - Forking for personal learning
+# - Submitting improvements via pull requests
+#
+# Not Allowed:
+# - Claiming this code as your own
+# - Re-uploading without credit or permission
+# - Selling or using commercially
+#
+# Contact for permissions:
+# Email: akp954834@gmail.com
+
+
 from pyrogram import Client
 import asyncio
 import config
@@ -153,3 +175,14 @@ class Userbot(Client):
                 await client.stop()
         except:
             pass
+
+# ©️ Copyright Reserved - @ZoxxOP  Akash Dakshwanshi
+
+# ===========================================
+# ©️ 2025 Akash Dakshwanshi (aka @ZoxxOP)
+# 🔗 GitHub : https://github.com/ZoxxOP/AnanyaMusic
+# 📢 Telegram Channel : https://t.me/AnanyaBots
+# ===========================================
+
+
+# ❤️ Love From AnanyaBots
