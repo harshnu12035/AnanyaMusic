8aaaeae378813732fd014edfe0adf4aa054acc76
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-11-10 14:37:22 +0530

    Update thumbnails.py

--- AnanyaMusic/utils/thumbnails.py
+++ AnanyaMusic/utils/thumbnails.py
@@ -200,9 +200,9 @@ async def gen_thumb(videoid: str):
         background = Image.blend(background, gradient_image, alpha=0.2)
         
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("ShrutiMusic/assets/font2.ttf", 30)
-        font = ImageFont.truetype("ShrutiMusic/assets/font.ttf", 30)
-        title_font = ImageFont.truetype("ShrutiMusic/assets/font3.ttf", 45)
+        arial = ImageFont.truetype("AnanyaMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("AnanyaMusic/assets/font.ttf", 30)
+        title_font = ImageFont.truetype("AnanyaMusic/assets/font3.ttf", 45)
 
 
         circle_thumbnail = crop_center_circle(youtube, 400, 20, start_gradient_color)
@@ -252,7 +252,7 @@ async def gen_thumb(videoid: str):
         draw_text_with_shadow(background, draw, (text_x_position, 400), "00:00", arial, (255, 255, 255))
         draw_text_with_shadow(background, draw, (1080, 400), duration, arial, (255, 255, 255))
         
-        play_icons = Image.open("ShrutiMusic/assets/play_icons.png")
+        play_icons = Image.open("AnanyaMusic/assets/play_icons.png")
         play_icons = play_icons.resize((580, 62))
         background.paste(play_icons, (text_x_position, 450), play_icons)
 
